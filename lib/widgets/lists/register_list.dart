00444f326c72f163dc57e2aebc9f90dacb60094b
n <nazmiahmad9@gmail.com>
2022-04-08 15:52:55 +0200

    fixed jobs list

--- lib/widgets/lists/register_list.dart
+++ lib/widgets/lists/register_list.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:timer_builder/timer_builder.dart';
 import 'package:tollo2/models/job.dart';
 import 'package:tollo2/models/track_unit.dart';
-import 'package:tollo2/services/formatters/date_full.dart';
-import 'package:tollo2/services/formatters/duration_formatter_in_days.dart';
 import 'package:tollo2/widgets/components/CustomCard.dart';
 import 'package:tollo2/widgets/components/stateful_counter.dart';
+
+import '../../services/formatters/date_full.dart';
+import '../../services/formatters/duration_formatter_in_days.dart';
 
 class RegisterNotesList extends StatefulWidget {
   const RegisterNotesList({Key? key, required this.job}) : super(key: key);
@@ -18,112 +18,142 @@ class RegisterNotesList extends StatefulWidget {
 }
 
 class _RegisterNotesListState extends State<RegisterNotesList> {
+  final bool animate = false;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    var indent2 = size.shortestSide / 10;
     List<TimeTrackUnit> register =
         widget.job.getAllTimeUnits().reversed.toList();
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ConstrainedBox(
-              constraints: BoxConstraints(maxHeight: size.longestSide / 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () => Navigator.pop(context),
-                    icon: Icon(CupertinoIcons.back),
-                  ),
-                ],
+            buildConstrainedBoxForBack(size, context),
+            totalCounter(size),
+            SizedBox(
+              height: size.longestSide * 0.76,
+              child: ListView.builder(
+                itemCount: register.length,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  TimeTrackUnit ttu = register[index];
+
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(4.0, 16.0, 8.0, 1.0),
+                          child: Row(
+                            children: [
+                              (ttu.note != null)
+                                  ? Text(
+                                      ttu.note!.note,
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          decoration: TextDecoration.underline,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  : Text('Not Closed yet',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: (Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText1!
+                                                  .fontSize)! *
+                                              1.2))
+                            ],
+                          ),
+                        ),
+                        Card(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  child: Text(
+                                    formatFull(ttu.begin),
+                                    style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  child: TimerBuilder.periodic(
+                                      Duration(seconds: 1),
+                                      //updates every second
+                                      builder: (context) {
+                                    return Text(
+                                      durationFormatter(
+                                        ttu.duration(),
+                                      ),
+                                    );
+                                  }),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Divider(
+                            thickness: size.aspectRatio * 4,
+                            color: Color(widget.job.categoryColor),
+                            indent: indent2,
+                            endIndent: indent2),
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CustomCard(
-                    widget: StatefulCounter(
-                      job: widget.job,
-                      scale: size.aspectRatio * 10,
-                    ),
-                    size: size),
-              ],
-            ),
-            CustomCard(
-                widget: Column(
-                  children: [
-                    SizedBox(
-                      height: size.longestSide / 2,
-                      child: ListView.builder(
-                        itemCount: register.length,
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (context, index) {
-                          TimeTrackUnit ttu = register[index];
-                          return Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Expanded(
-                                    flex: 2,
-                                    child: FittedBox(
-                                      fit: BoxFit.scaleDown,
-                                      child: Text(
-                                        formatFull(ttu.begin),
-                                        style: TextStyle(
-                                            fontStyle: FontStyle.italic),
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    flex: 1,
-                                    child: FittedBox(
-                                      fit: BoxFit.scaleDown,
-                                      child: TimerBuilder.periodic(
-                                          Duration(seconds: 1),
-                                          //updates every second
-                                          builder: (context) {
-                                        return Text(
-                                          durationFormatter(
-                                            ttu.duration(),
-                                          ),
-                                        );
-                                      }),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    flex: 5,
-                                    child: FittedBox(
-                                      fit: BoxFit.scaleDown,
-                                      child: (ttu.note != null)
-                                          ? Text(
-                                              ttu.note!.note,
-                                              textAlign: TextAlign.left,
-                                            )
-                                          : Text('Not Closed yet'),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Divider(),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-
-                size: size),
           ],
         ),
       ),
     );
   }
+
+  Row totalCounter(Size size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        CustomCard(
+            widget: StatefulCounter(
+              job: widget.job,
+              scale: size.aspectRatio * 10,
+            ),
+            size: size),
+      ],
+    );
+  }
+
+  ConstrainedBox buildConstrainedBoxForBack(Size size, BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxHeight: size.longestSide / 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(CupertinoIcons.back),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// Sample time series data type.
+class TimeSeriesSales {
+  final DateTime time;
+  late final Duration sales;
+
+  TimeSeriesSales(this.time, this.sales);
 }
